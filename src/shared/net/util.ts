cec4c61f524d0ebce8ea489a1ed61f14e6c5967d
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-03-03 11:51:50 -0500

    Move to builds repo

--- src/shared/net/util.ts
+++ src/shared/net/util.ts
@@ -1,10 +1,15 @@
-const oldPathRegex = /^https:\/\/raw.githubusercontent\.com\/Gimloader\/client-plugins\/(?:refs\/heads\/)?main\/(plugins|libraries)(?:\/\w+\/build)?\/(\w+\.js)$/g;
+const oldStructureRegex = /^https:\/\/raw.githubusercontent\.com\/Gimloader\/client-plugins\/(?:refs\/heads\/)?main\/(plugins|libraries)(?:\/\w+\/build)?\/(\w+\.js)$/g;
+const oldRepoRegex = /^https:\/\/raw.githubusercontent\.com\/Gimloader\/client-plugins\/(?:refs\/heads\/)?main\/build\/(plugins|libraries)\/(\w+\.js)$/g;
+
 export function formatDownloadUrl(url: string) {
     // migrate from TheLazySquid/Gimloader -> Gimloader/client-plugins
     url = url.replace("https://raw.githubusercontent.com/TheLazySquid/Gimloader", "https://raw.githubusercontent.com/Gimloader/client-plugins");
 
     // migrate structure of client-plugins repo
-    url = url.replace(oldPathRegex, "https://raw.githubusercontent.com/Gimloader/client-plugins/main/build/$1/$2");
+    url = url.replace(oldStructureRegex, "https://raw.githubusercontent.com/Gimloader/builds/main/$1/$2");
+
+    // migrate from client-plugins to builds repo
+    url = url.replace(oldRepoRegex, "https://raw.githubusercontent.com/Gimloader/builds/main/$1/$2");
 
     return url;
 }
